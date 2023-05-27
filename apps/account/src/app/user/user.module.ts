67539668ce1790fbbbbec700058672cbd7bf3a7b
Brometey <barstar35@gmail.com>
2023-05-27 10:05:01 +0600

    user models, db init

--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './models/user.model';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature(
+    [{ name: User.name, schema: UserSchema}]
+  )],
+  providers: [UserService],
+})
 export class UserModule {}
